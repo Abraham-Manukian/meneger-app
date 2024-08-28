4a8ec7bef87bd7239745519d4653b0aae6032289
fronttmp <fronttmp@nomia.net>
2024-08-28 09:28:22 +0000

    Revert "test"
    This reverts commit b77f11e9a13faea34b58f04bed740c2e566670a8

--- pos/app/src/main/java/net/nomia/pos/ui/onboarding/Onboarding.kt
+++ pos/app/src/main/java/net/nomia/pos/ui/onboarding/Onboarding.kt
@@ -1,57 +1,31 @@
 package net.nomia.pos.ui.onboarding
 
-import android.app.Activity
-import androidx.compose.foundation.clickable
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
-import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
-import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import net.nomia.common.ui.composable.NomiaScrollableScaffold
-import net.nomia.common.ui.theme.appResources
+import net.nomia.common.ui.composable.ScreenTitleText
 import net.nomia.pos.R
 
-@OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
+
 @Composable
 fun Onboarding(
     viewModel: OnboardingViewModel = hiltViewModel()
 ) {
-    val activity = (LocalContext.current as Activity)
-    val windowSizeClass = calculateWindowSizeClass(activity)
-
-    when (windowSizeClass.widthSizeClass) {
-        WindowWidthSizeClass.Compact,
-        WindowWidthSizeClass.Medium-> {
-            // Layout for smaller screens (most phones in portrait)
-        }
-        WindowWidthSizeClass.Expanded -> {
-            // Layout for larger screens (tablets, desktops)
-        }
-    }
-
     NomiaScrollableScaffold(
-        title = {
-            Icon(
-                painter = painterResource(id = MaterialTheme.appResources.textLogoResId),
-                tint = Color.Unspecified,
-                contentDescription = null
-            )
-        },
+        title = { ScreenTitleText(text = stringResource(id = R.string.onboarding)) },
         actions = {
-            Text(
-                stringResource(R.string.skip),
-                modifier = Modifier.clickable {
-
-                }
-            )
+            IconButton(
+                onClick = viewModel::logOut
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_logout_24),
+                    contentDescription = null
+                )
+            }
         },
     ) {
         /**
